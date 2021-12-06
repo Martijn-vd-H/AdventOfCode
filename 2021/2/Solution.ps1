4f105bcbe3ebdc663797571ed6bc8bf3f72279a4
Martijn van den Hoven <martijnh@Uptrends.com>
2021-12-06 14:44:24 +0100

    Solution part 1 of day 2

--- 2021/2/Solution.ps1
+++ 2021/2/Solution.ps1
@@ -1,10 +1,22 @@
-$Position
-$Depth
+$position = 0
+$depth = 0
 
-$inputValues = Get-Content "testinput.txt"
+$inputValues = Get-Content "input.txt"
+$regexPattern = "(forward|down|up){1}\s(\d+)"
 foreach($value in $inputValues){
-    $value -match "(forward|depth){1}\s(\d+)"
-    Write-Host "Match 0" $Matches[0]
-    Write-Host "Match 1" $Matches[1]
-    Write-Host "Match 2" $Matches[2]
+    $value -match $regexPattern
+    $direction = $Matches[1]
+    $distance = $Matches[2]
+    if($direction -eq "forward"){
+        $position += $distance
+    }
+    if($direction -eq "up"){
+        $depth -= $distance
+    }
+    if ($direction -eq "down"){
+        $depth += $distance
+    }
 }
+
+Write-Host "The final position is" $position "and the final depth is" $depth
+Write-Host "The solution is " ($depth * $position)
