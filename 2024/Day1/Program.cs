bd546cbac203d1eb75ef71c3a2f90d4f4e068ff6
Martijn van den Hoven <mvandenhoven@vitas.nl>
2024-12-02 22:06:12 +0100

    Solve day 1 part 2

--- 2024/Day1/Program.cs
+++ 2024/Day1/Program.cs
@@ -1,21 +1,45 @@
-﻿var total = 0;
-var list1 = new List<int>();
-var list2 = new List<int>();
+﻿using FluentAssertions;
 
-foreach (var line in System.IO.File.ReadLines("InputPart1"))
+List<int> list1;
+List<int> list2;
+
+CalculateTotalDistance("TestInput1").Should().Be(11);
+CalculateTotalDistance("InputPart1").Should().Be(1506483);
+
+CalculateSimilarity("TestInput1").Should().Be(31);
+CalculateSimilarity("InputPart1").Should().Be(31);
+Console.WriteLine("Great succes!");
+return;
+
+// Part1
+int CalculateTotalDistance(string filename)
 {
-    var numbers = line.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
-    list1.Add(numbers[0]);
-    list2.Add(numbers[1]);
+    ReadLists(filename);
+    
+    list1.Sort();
+    list2.Sort();
+
+    return list1.Select((t, i) => new[] { t, list2[i] })
+        .Sum(numbers => numbers.Max() - numbers.Min());
 }
 
-list1.Sort();
-list2.Sort();
-
-for (var i = 0; i < list1.Count; i++)
+void ReadLists(string filename)
 {
-    var numbers = new[] { list1[i], list2[i] };
-    total += numbers.Max() - numbers.Min();
+    list1 = [];
+    list2 = [];
+    
+    foreach (var line in System.IO.File.ReadLines(filename))
+    {
+        var numbers = line.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+        list1.Add(numbers[0]);
+        list2.Add(numbers[1]);
+    }
+
 }
 
-Console.WriteLine(total);
+// Part2
+int CalculateSimilarity(string filename)
+{
+    ReadLists(filename);
+    return (list1.Select(item => new { item, count = list2.Count(a => a.Equals(item)) }).Select(@t => @t.count * @t.item)).Sum();
+}
