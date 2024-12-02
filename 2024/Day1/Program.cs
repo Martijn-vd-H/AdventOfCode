2eced4b704e629062b8c47a2dd6af55edeeb04f4
Martijn van den Hoven <mvandenhoven@vitas.nl>
2024-12-02 21:48:37 +0100

    Solve day 1 part 1

---
+++ 2024/Day1/Program.cs
@@ -0,0 +1,21 @@
+﻿var total = 0;
+var list1 = new List<int>();
+var list2 = new List<int>();
+
+foreach (var line in System.IO.File.ReadLines("InputPart1"))
+{
+    var numbers = line.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+    list1.Add(numbers[0]);
+    list2.Add(numbers[1]);
+}
+
+list1.Sort();
+list2.Sort();
+
+for (var i = 0; i < list1.Count; i++)
+{
+    var numbers = new[] { list1[i], list2[i] };
+    total += numbers.Max() - numbers.Min();
+}
+
+Console.WriteLine(total);
