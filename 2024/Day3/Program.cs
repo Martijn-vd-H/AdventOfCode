a7b806ef5f563feac294d491366c581e07c0ad30
Martijn van den Hoven <mvandenhoven@vitas.nl>
2024-12-03 09:37:08 +0100

    Solve day 3 part 2

--- 2024/Day3/Program.cs
+++ 2024/Day3/Program.cs
@@ -3,10 +3,12 @@ using FluentAssertions;
 
 CalculateMultiplications("TestInput1").Should().Be(161);
 CalculateMultiplications("InputPart1").Should().Be(157621318);
-
+CalculateMultiplicationsWithConditionals("TestInput2").Should().Be(48);
+CalculateMultiplicationsWithConditionals("InputPart1").Should().Be(79845780);
 Console.WriteLine("Done!");
 return;
 
+// Part 1
 int CalculateMultiplications(string filename)
 {
     var input = System.IO.File.ReadAllText(filename);
@@ -15,6 +17,36 @@ int CalculateMultiplications(string filename)
     foreach (Match match in matches)
     {
         total += int.Parse(match.Groups[1].Value) * int.Parse(match.Groups[2].Value);
+    }
+
+    return total;
+}
+
+// Part 2
+int CalculateMultiplicationsWithConditionals(string filename)
+{
+    var input = System.IO.File.ReadAllText(filename);
+    var matches = Regex.Matches(input, @"mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)");
+    var total = 0;
+    var enabled = true;
+    foreach (Match match in matches)
+    {
+        if (match.Value.Equals("don't()"))
+        {
+            enabled = false;
+            continue;
+        }
+
+        if (match.Value.Equals("do()"))
+        {
+            enabled = true;
+            continue;
+        }
+
+        if (enabled)
+        {
+            total += int.Parse(match.Groups[1].Value) * int.Parse(match.Groups[2].Value);
+        }
     }
 
     return total;
