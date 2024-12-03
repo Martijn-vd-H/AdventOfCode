72eafe32c3d3bffea4076222d6bf9c0b3ab04d0f
Martijn van den Hoven <mvandenhoven@vitas.nl>
2024-12-03 20:08:45 +0100

    Use Benchmark.Net for day1

--- 2024/Day1/Program.cs
+++ 2024/Day1/Program.cs
@@ -1,45 +1,75 @@
-﻿using FluentAssertions;
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using FluentAssertions;
 
-List<int> list1;
-List<int> list2;
+var result = Reader.ReadLists("TestInput1");
+Calculators.CalculateTotalDistance(result.Item1, result.Item2).Should().Be(11);
+Calculators.CalculateSimilarity(result.Item1, result.Item2).Should().Be(31);
 
-CalculateTotalDistance("TestInput1").Should().Be(11);
-CalculateTotalDistance("InputPart1").Should().Be(1506483);
+result = Reader.ReadLists("InputPart1");
+Calculators.CalculateTotalDistance(result.Item1, result.Item2).Should().Be(1506483);
+Calculators.CalculateSimilarity(result.Item1, result.Item2).Should().Be(23126924);
 
-CalculateSimilarity("TestInput1").Should().Be(31);
-CalculateSimilarity("InputPart1").Should().Be(23126924);
 Console.WriteLine("Great succes!");
+
+BenchmarkRunner.Run<CalculatorBenchmarks>();
+
 return;
 
-// Part1
-int CalculateTotalDistance(string filename)
-{
-    ReadLists(filename);
-    
-    list1.Sort();
-    list2.Sort();
 
-    return list1.Select((t, i) => new[] { t, list2[i] })
-        .Sum(numbers => numbers.Max() - numbers.Min());
-}
 
-void ReadLists(string filename)
+public class CalculatorBenchmarks
 {
-    list1 = [];
-    list2 = [];
-    
-    foreach (var line in System.IO.File.ReadLines(filename))
+    private (List<int>, List<int>) Data; 
+        
+    public CalculatorBenchmarks()
     {
-        var numbers = line.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
-        list1.Add(numbers[0]);
-        list2.Add(numbers[1]);
+        Data = Reader.ReadLists("InputPart1");
     }
-
+    
+    [Benchmark]
+    public void Part1()
+    {
+        Calculators.CalculateSimilarity(Data.Item1, Data.Item2);
+    }
 }
 
-// Part2
-int CalculateSimilarity(string filename)
+internal class Calculators
 {
-    ReadLists(filename);
-    return (list1.Select(item => new { item, count = list2.Count(a => a.Equals(item)) }).Select(@t => @t.count * @t.item)).Sum();
+    // Part1
+    internal static int CalculateTotalDistance(List<int> list1, List<int> list2)
+    {
+        list1.Sort();
+        list2.Sort();
+
+        return list1.Select((t, i) => new[] { t, list2[i] })
+            .Sum(numbers => numbers.Max() - numbers.Min());
+    }
+    
+    // Part2
+    internal static int CalculateSimilarity(List<int> list1, List<int> list2)
+    {
+        return list1
+            .Select(item => new { item, count = list2.Count(a => a.Equals(item)) })
+            .Select(@t => @t.count * @t.item)
+            .Sum();
+    }
+}
+
+internal class Reader
+{
+    internal static (List<int>, List<int>) ReadLists(string filename)
+    {
+        var list1 = new List<int>();
+        var list2 = new List<int>();
+        
+        foreach (var line in File.ReadLines(filename))
+        {
+            var numbers = line.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            list1.Add(numbers[0]);
+            list2.Add(numbers[1]);
+        }
+
+        return (list1, list2);
+    }
 }
