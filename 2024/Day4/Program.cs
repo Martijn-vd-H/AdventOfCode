8a8c31c54aa21a00da405963c679edfab11a13ad
Martijn van den Hoven <mvandenhoven@vitas.nl>
2024-12-05 16:35:43 +0100

    Simplify code in preparation of part 2

--- 2024/Day4/Program.cs
+++ 2024/Day4/Program.cs
@@ -1,53 +1,59 @@
-﻿// Alle regels lezen
-var path = "InputPart1";
-var lines = System.IO.File.ReadAllLines(path);
+﻿using System.Reflection.PortableExecutable;
+using FluentAssertions;
 
-// Langste regel tellen
-var length = lines.Max(line => line.Length);
+FindXmas(File.ReadAllLines("TestInput1")).Should().Be(18);
+FindXmas(File.ReadAllLines("InputPart1")).Should().Be(2560);
 
-var totalCount = 0;
-
-var offsetPlans = new List<OffsetPlan>
+// Part 1
+int FindXmas(string[] strings)
 {
-    new(new Offset(0, 0), new Offset(1, 0), new Offset(2, 0), new Offset(3, 0)), // right
-    new(new Offset(0, 0), new Offset(-1, 0), new Offset(-2, 0), new Offset(-3, 0)), // left
-    new(new Offset(0, 0), new Offset(0, 1), new Offset(0, 2), new Offset(0, 3)), // up
-    new(new Offset(0, 0), new Offset(0, -1), new Offset(0, -2), new Offset(0, -3)), // down
-    new(new Offset(0, 0), new Offset(-1, 1), new Offset(-2, 2), new Offset(-3, 3)), // leftup
-    new(new Offset(0, 0), new Offset(1, 1), new Offset(2, 2), new Offset(3, 3)), // rightup
-    new(new Offset(0, 0), new Offset(-1, -1), new Offset(-2, -2), new Offset(-3, -3)), // leftdown
-    new(new Offset(0, 0), new Offset(1, -1), new Offset(2, -2), new Offset(3, -3)), // rightdown
-};
+    // Langste regel tellen
+    var length = strings.Max(line => line.Length);
 
+    var totalCount = 0;
 
-for (int verticalIndex = 0; verticalIndex < lines.Length; verticalIndex++)
-{
-    var line = lines[verticalIndex];
-    for (int horizontalIndex = 0; horizontalIndex < length; horizontalIndex++)
+    var offsetPlans = new List<OffsetPlan>
     {
-        if (line[horizontalIndex] == 'X')
-        {
-            foreach (var offsetPlan in offsetPlans)
-            {
-                try
-                {
-                    string sub = "" + lines[verticalIndex + offsetPlan.one.y][horizontalIndex + offsetPlan.one.x] +
-                    lines[verticalIndex + offsetPlan.two.y][horizontalIndex + offsetPlan.two.x] +
-                    lines[verticalIndex + offsetPlan.three.y][horizontalIndex + offsetPlan.three.x] +
-                    lines[verticalIndex + offsetPlan.four.y][horizontalIndex + offsetPlan.four.x];
+        new(new Offset(0, 0), new Offset(1, 0), new Offset(2, 0), new Offset(3, 0)), // right
+        new(new Offset(0, 0), new Offset(-1, 0), new Offset(-2, 0), new Offset(-3, 0)), // left
+        new(new Offset(0, 0), new Offset(0, 1), new Offset(0, 2), new Offset(0, 3)), // up
+        new(new Offset(0, 0), new Offset(0, -1), new Offset(0, -2), new Offset(0, -3)), // down
+        new(new Offset(0, 0), new Offset(-1, 1), new Offset(-2, 2), new Offset(-3, 3)), // leftup
+        new(new Offset(0, 0), new Offset(1, 1), new Offset(2, 2), new Offset(3, 3)), // rightup
+        new(new Offset(0, 0), new Offset(-1, -1), new Offset(-2, -2), new Offset(-3, -3)), // leftdown
+        new(new Offset(0, 0), new Offset(1, -1), new Offset(2, -2), new Offset(3, -3)), // rightdown
+    };
 
-                    if (sub.Equals("XMAS")) totalCount++;
-                }
-                catch (IndexOutOfRangeException)
+
+    for (int verticalIndex = 0; verticalIndex < strings.Length; verticalIndex++)
+    {
+        var line = strings[verticalIndex];
+        for (int horizontalIndex = 0; horizontalIndex < length; horizontalIndex++)
+        {
+            if (line[horizontalIndex] == 'X')
+            {
+                foreach (var offsetPlan in offsetPlans)
                 {
-                    continue;
+                    try
+                    {
+                        var subString = "";
+                        foreach (var offset in offsetPlan.offsets)
+                        {
+                            subString += strings[verticalIndex + offset.y][horizontalIndex + offset.x];
+                        }
+                        if (subString.Equals("XMAS")) totalCount++;
+                    }
+                    catch (IndexOutOfRangeException)
+                    {
+                        continue;
+                    }
                 }
             }
         }
     }
+
+    return totalCount;
 }
 
-System.Console.WriteLine(totalCount);
-
-record OffsetPlan(Offset one, Offset two, Offset three, Offset four);
+record OffsetPlan(params Offset[] offsets);
 record Offset(int x, int y);
